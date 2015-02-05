58b0bceaeaf5c3f784fea26be1ba78fed23e81d5
Alex <chairmanmow@users.noreply.github.com>
2015-02-05 12:02:33 -0700

    Update 16colors.js

--- 16colors.js
+++ 16colors.js
@@ -1,11 +1,10 @@
-// The following comment on the next tthree lines shows the progression of how functions are executed in this program for the typical use case.
-
 // main() -->
 // setModemSpeed() --> selectAYear()--> browsePacksInYear() --> findPagesInAYear() --> displayPackPage() 
 // --> selectAPack() --> getListFromPack() -->  askForAnsiFromPack() --> grabAnsi() --> showAnsi();
 
 load("sbbsdefs.js");
 load("http.js");
+load("json-client.js");
 
 console.clear();
 console.attributes = BG_BLACK|WHITE;
@@ -18,7 +17,7 @@ var thePacks;
 var currentPack;
 var currentPackList = [];
 var currentAnsiFile;
-var modemSpeed = 9;  //basically the length of chunks to be sent at a time 
+var modemSpeed = 9;
 
 menuOptions = [{hotkey:"Y", description:"Select a Year"},{hotkey:"P", description:"Select Pack from Current Year " + theYear},{hotkey:"B", description:"Browse Ansi's in Pack " + currentPack},{hotkey:"X", description:"Exit Program"}]
 
@@ -43,157 +42,54 @@ try{
 	}
 }
 
-function setModemSpeed(){
-	console.putmsg("\r\n\1h\1cEnter Your Simulated Modem Speed.\r\n\r\n");
-	choice = -1;
-	while(choice < 0 || choice > 128){
-		console.putmsg("\1h\1mCurrent Speed\1g=\1r "+ modemSpeed+ "000\1c bps\r\n\r\n\1h\1yEnter a number from \1w1 \1yto \1w128 \1y or Enter to keep current speed\1r--> ");
-		choice = console.getnum();
-	}
-	if(choice > 0){
-	modemSpeed = parseInt(choice);
-} 
+//this function sets the global modemSpeed var
+function setModemSpeed(speed){
+		if(speed == undefined  || speed < 1){
+			console.putmsg("\r\n\1h\1cEnter Your Simulated Modem Speed.\r\n\r\n");
+			speed = -1;
+				while(speed < 0 || speed > 128){
+					console.putmsg("\1h\1mCurrent Speed\1g=\1r "+ modemSpeed+ "000\1c bps\r\n\r\n\1h\1yEnter a number from \1w1 \1yto \1w128 \1y or Enter to keep current speed\1r--> ");
+					speed = console.getnum();
+					if(speed <= 0){
+						speed = modemSpeed;
+					}
+				}
+		} 
+	modemSpeed = parseInt(speed);
 	console.putmsg("\r\n\1h\1wModem Speed set to \1r" + modemSpeed + "000 \1ybps\r\n")
 }
 
-
+//a prompt that gets displayed to remind you what you're browsing and at whatspeed
 function showGlobalPrompt() {
 	packPromptStr = "";
-	if(currentPack == ""){
-		packPromptStr = "\1h\1rYou must select a Pack!";
-	} else {
-		packPromptStr = currentPack;
-	}
-	console.putmsg("\r\n\1h\1m16colors.net archive \1y**\1r" + modemSpeed + "000\1wbps\1y**: \1cYear: \1w" + theYear + " \1cPack: \1w" + packPromptStr + "\r\n");
-}
-// 1. //http://api.sixteencolors.net/v0/year  -- get list of years
-// Sample Data // {"packs":690,"year":1997}
-
-function selectAYear(){
-		console.putmsg("\r\n\1h\1wSelect a Year\r\n");
-		choice = -1;
-		while(choice < 1990 || choice > 2015){
-			console.putmsg("\1h\1cEnter a year from 1990 to 2015\1y--> " );
-			choice = console.getnum();
-		}
-		theYear = choice;
-		currentPack = "";
-		browsePacksInYear(theYear);
+		if(currentPack == ""){ packPromptStr = "\1h\1rYou must select a Pack!"; } else { packPromptStr = currentPack;}
+		console.putmsg("\r\n\1h\1m16colors.net archive \1y**\1r" + modemSpeed + "000\1wbps\1y**: \1cYear: \1w" + theYear + " \1cPack: \1w" + packPromptStr + "\r\n");
 }
 
-// 2. //http://api.sixteencolors.net/v0/year/1997 -- grab a year (this one has over 600 paks![pagination])
-// sample return data: need name // [{"name":"01ninja","filename":"01ninja.zip","groups":[],"year":1997,"month":null},
- 
-
-function selectAPack(){
-		console.putmsg("\r\n\1rSelect a Pack Number.  ");
-		choice = -1;
-		var packLength = thePacks.length - 1;
-		while(choice < 0 || choice > packLength){
-			console.putmsg("\1yEnter a number from 0 to " + packLength);
-			choice = console.getnum();
+//selects the year that you want to look at packs from.
+function selectAYear(aYear){
+		if(aYear == undefined  || aYear < 1990 || aYear > 2015){
+			console.putmsg("\r\n\1h\1wSelect a Year\r\n");
+			aYear = -1;
+				while(aYear < 1990 || aYear > 2015){
+					console.putmsg("\1h\1cEnter a year from 1990 to 2015\1y--> " );
+					aYear = console.getnum();
+				}
 		}
-		currentPack = thePacks[choice].name;
-		askForAnsiFromPack(currentPack);
-	}
+	theYear = aYear;
+	currentPack = "";  //there is a switch that relies on currentPack being used as a blank string
+	browsePacksInYear(theYear); 
+}
 
+//looks at packs from a given year
 function browsePacksInYear(aYear){
 	currentPage = 1;
 	numberOfPages = findPagesInYear(aYear);
-
-	
 	displayPackPage();
-		while(currentPack == ""){
-		pageNavPrompt();
-
-		}
-
-
+		while(currentPack == ""){ pageNavPrompt();}
 }
 
-function displayPackPage(){
-			for(i=0;i<thePacks.length;i++){
-			var aPack = thePacks[i];
-			console.putmsg("\1g " + i + " \1cName \1r -- \1h\1y " + aPack.name +"\r\n" ); //+ "\1y yearr:\1h\1m " + aPack.year + "\r\n"
-		}
-}
-function pageNavPrompt(){
-	pageNavOptions = [{hotkey:"S", description:"Select Pack and List Files"},{hotkey:"N", description:"Next Page of Packs"},{hotkey:"P", description:"Previous Page of Packs"},{hotkey:"J", description:"Jump to Page Number"},{hotkey:"Y", description:"Select a Year"},{hotkey:"M", description:"Set Modem Speed"},{hotkey:"X", description:"Exit Program"}]
-	showGlobalPrompt();
-
-	console.putmsg("\1m Enter a choice ")
-	for(i=0;i<pageNavOptions.length;i++){
-		console.putmsg("\1g" + pageNavOptions[i].hotkey +"\1h\1y, " )
-	}
-	console.putmsg("\1h ?\1i\1r:\1n\r\nPage \1h\1y" + currentPage + "\1n of \1y" + numberOfPages);
-	var pagePromptSel = console.getstr();
-	if(parseInt(pagePromptSel) < thePacks.length && parseInt(pagePromptSel) >= 0) {
-		selectAPack2(parseInt(pagePromptSel));
-	} else { switch(pagePromptSel.toUpperCase()){
-			case "S" :
-			currentPack = "";
-				browsePacksInYear(theYear);
-				return;
-			case "N" :
-				if(currentPage + 1 <= numberOfPages){
-					currentPage++;
-					checkPageListing();
-					displayPackPage();
-					return;
-				} else {
-						console.putmsg("\1rLast Page Reached!\r\n");
-						break;
-				}
-			case "P" :
-				if(currentPage - 1 > 0){
-					currentPage--;
-					checkPageListing();
-					displayPackPage();
-					return;
-				} else {
-					console.putmsg("\1rYou are on the first page!\r\n")
-						break;
-				}
-			case "J" :
-				choice2 = -1;
-				while(choice2 < 1 || choice2 > numberOfPages){
-					console.putmsg("\1h\1rSelect a Page number \1y--> ");
-					choice2 = console.getnum();
-				}
-				currentPage = parseInt(choice2);
-				checkPageListing();
-				displayPackPage();
-			return;
-			case "Y" :
-				selectAYear();
-				return;
-				case "?" :
-				console.putmsg("\r\n");
-				for(i=0;i<pageNavOptions.length;i++){
-					console.putmsg(pageNavOptions[i].hotkey + " --- " + pageNavOptions[i].description + "\r\n" )
-				}
-				break;
-			case "M" :
-				setModemSpeed();
-				break;
-			case "X" :
-				throw("quit");
-			default :
-				console.putmsg("Enter a Valid Selection\r\n");
-				break;
-		}
-	}
-
-
-
-function checkPageListing(){
-		request2 = new HTTPRequest();
-		completeYearEndpoint = apiRoot + "/year/" + theYear + "?page=" + currentPage + "&rows=30";	
-		entireYearResponse = request2.Get(completeYearEndpoint);
-		thePacks = JSON.parse(request2.body);
-	}
-
-}
+//This is an http function to find out how many pages worth of packs there are in a year.
 
 function findPagesInYear(aYear){
 	theYear = aYear;
@@ -223,14 +119,103 @@ function findPagesInYear(aYear){
 	console.putmsg("\r\n\1h\1w" + theYear + "\1m\1h Number of Pages : \1h " + numberOfPages + " \1yApproximate Packs : " + approxResults + "-" + numberOfPages * 30 + " \r\n\r\n");
 	return numberOfPages;
 }
+//this is an http function to check a page besides the first one.
+function checkPageListing(){
+		request2 = new HTTPRequest();
+		completeYearEndpoint = apiRoot + "/year/" + theYear + "?page=" + currentPage + "&rows=30";	
+		entireYearResponse = request2.Get(completeYearEndpoint);
+		thePacks = JSON.parse(request2.body);
+}
 
 
-// PART THREE CHECK FILES IN A PACK GIVEN A STRING WITH THE PACK NAME AND RETRIEVE THE FILE AND DISPLAY IT
 
-// 3. http://api.sixteencolors.net/v0/pack/01ninja -- Get pack details and file contents  -
-//{"fullsize":"/pack/01ninja/FZ-BLUE.ANS/fullscale","uri":"/pack/01ninja/FZ-BLUE.ANS","filename":"FZ-BLUE.ANS","pack":{"uri":"/pack/01ninja","name":"01ninja","filename":"01ninja.zip"},
-//"file_location":"/pack/01ninja/FZ-BLUE.ANS/download","thumbnail":"/pack/01ninja/FZ-BLUE.ANS/preview"},{
+//display the packs from a year as a list, etc files in a directory
+function displayPackPage(){
+		for(i=0;i<thePacks.length;i++){
+			var aPack = thePacks[i];
+			console.putmsg("\1g " + i + " \1cName \1r -- \1h\1y " + aPack.name +"\r\n" ); //+ "\1y yearr:\1h\1m " + aPack.year + "\r\n"
+		}
+}
+//really messy function that controls navigating through a year.
+function pageNavPrompt(){
+	showGlobalPrompt();
+	console.putmsg("\1m Enter a choice ")
+	pageNavOptions = [{hotkey:"S", description:"Select Pack and List Files"},{hotkey:"N", description:"Next Page of Packs"},{hotkey:"P", description:"Previous Page of Packs"},{hotkey:"J", description:"Jump to Page Number"},{hotkey:"Y", description:"Select a Year"},{hotkey:"M", description:"Set Modem Speed"},{hotkey:"X", description:"Exit Program"}]
 
+		for(i=0;i<pageNavOptions.length;i++){
+			console.putmsg("\1g" + pageNavOptions[i].hotkey +"\1h\1y, " )
+		}
+
+	console.putmsg("\1h ?\1i\1r:\1n\r\nPage \1h\1y" + currentPage + "\1n of \1y" + numberOfPages);
+	var pagePromptSel = console.getstr();
+
+		if(parseInt(pagePromptSel) < thePacks.length && parseInt(pagePromptSel) >= 0) {
+			selectAPack(parseInt(pagePromptSel));
+		} else { 
+			switch(pagePromptSel.toUpperCase()){
+				case "S" :
+				currentPack = "";
+					browsePacksInYear(theYear);
+					return;
+				case "N" :
+					if(currentPage + 1 <= numberOfPages){
+						currentPage++;
+						checkPageListing();
+						displayPackPage();
+						return;
+					} else {
+							console.putmsg("\1rLast Page Reached!\r\n");
+							break;
+					}
+				case "P" :
+					if(currentPage - 1 > 0){
+						currentPage--;
+						checkPageListing();
+						displayPackPage();
+						return;
+					} else {
+						console.putmsg("\1rYou are on the first page!\r\n")
+							break;
+					}
+				case "J" :
+					choice2 = -1;
+					while(choice2 < 1 || choice2 > numberOfPages){
+						console.putmsg("\1h\1rSelect a Page number \1y--> ");
+						choice2 = console.getnum();
+					}
+					currentPage = parseInt(choice2);
+					checkPageListing();
+					displayPackPage();
+				return;
+				case "Y" :
+					selectAYear();
+					return;
+					case "?" :
+					console.putmsg("\r\n");
+					for(i=0;i<pageNavOptions.length;i++){
+						console.putmsg(pageNavOptions[i].hotkey + " --- " + pageNavOptions[i].description + "\r\n" )
+					}
+					break;
+				case "M" :
+					setModemSpeed();
+					break;
+				case "X" :
+					throw("quit");
+				default :
+					console.putmsg("Enter a Valid Selection\r\n");
+					break;
+			}
+		}
+	}
+
+function selectAPack(packNum){
+	choice = packNum;
+	currentPack = thePacks[choice].name;
+	askForAnsiFromPack(currentPack);
+}
+
+
+//http function to get a list of files from a pack with optional extension filtering.
 function getListFromPack(pack,filterOff){
 	var request = new HTTPRequest();
 	var packRoute = apiRoot + "/pack/" + pack;
@@ -243,16 +228,16 @@ function getListFromPack(pack,filterOff){
 		for(i=0;i<ansiFiles.length;i++){
 			ansiFile = ansiFiles[i];
 			ansiFileExt = ansiFile.filename.substring(ansiFile.filename.length - 3,ansiFile.filename.length).toUpperCase();
-			if(filterOff == true){if(ansiFileExt != "EXE" && ansiFileExt != "COM" && ansiFileExt != "GIF" && ansiFileExt != "JPG"){ filteredFiles.push(ansiFile)}
-				} else {
-					if(ansiFileExt == "ASC" || ansiFileExt == "ANS" ||ansiFileExt == "TXT" ) {filteredFiles.push(ansiFile); }
+				if(filterOff == true){if(ansiFileExt != "EXE" && ansiFileExt != "COM" && ansiFileExt != "GIF" && ansiFileExt != "JPG"){ filteredFiles.push(ansiFile)}
+					} else {
+						if(ansiFileExt == "ASC" || ansiFileExt == "ANS" ||ansiFileExt == "TXT" ) {filteredFiles.push(ansiFile); }
 				}			
 		}
-
-
 	currentPackList = filteredFiles;
 	return filteredFiles;
 }
+
+//function to display files from preceding function getListFromPack()
 
 function displayPackList(pack){
 	console.clear();
@@ -265,6 +250,8 @@ function displayPackList(pack){
 		}
 }
 
+
+//after you selected a Pack this will be your prompt to choose an ANSI to display
 function askForAnsiFromPack(pack){
 	thePackList = getListFromPack(pack);
 	packListLength = thePackList.length - 1;
@@ -273,16 +260,11 @@ function askForAnsiFromPack(pack){
 		if(console.getkey(K_UPPER) ==  "Y"){
 			thePackList = getListFromPack(currentPack,true);
 			packListLength = thePackList.length - 1;
-			//displayPackList(): 
 		} else {
 			selectAYear();
 			return;
 		}
 	}
-	
-	var i;
-
-
 	displayPackList(pack);  //for convenience  interface TBD
 
 	while(1){
@@ -296,23 +278,7 @@ function askForAnsiFromPack(pack){
 			browsePacksInYear(theYear);
 			return;
 			}else if (choice.toUpperCase() == "A"){
-				console.putmsg("Enter \1h\1yY\1n to clear & pause between AnSIs");
-				var clearScreen = console.getkey().toUpperCase();
-				for(i=0;i<thePackList.length;i++){
-					if(clearScreen == "Y"){console.clear();}
-						
-					ansiToGet = thePackList[i];
-					console.putmsg("\r\n\1h\1g\r\n UPCOMING \1r *** \1y" + ansiToGet.file_location + " \1yOr***\1g BELOW\r\n\r\n\1n");	
-					grabAnsi(ansiToGet.file_location);
-					if(console.inkey(K_UPPER, 500) == "Q"){ 
-		                  	break; 
-		                  }
-					showAnsi();
-					console.putmsg("\r\n\1h\1c\r\n THAT WAS \1m *** \1w" + ansiToGet.file_location + " \1m***\1c ABOVE\r\n\r\n\1n");
-					
-					
-		                  //console.clear();
-	            }
+				showAllAnsisInPack();
 			}else if(choice.toUpperCase() == "D"){
 				displayPackList(pack); 
 			} else if(choice.toUpperCase() == "M"){
@@ -341,24 +307,32 @@ function askForAnsiFromPack(pack){
 	}
 }	
 
+//this function will go through all ANSI's in a pack and display them as more of a slidehosw
+function showAllAnsisInPack(){		
+	var i;
+	console.putmsg("Enter \1h\1yY\1n to clear & pause between AnSIs");
+	var clearScreen = console.getkey().toUpperCase();
+		for(i=0;i<thePackList.length;i++){
+				if(clearScreen == "Y"){console.clear();}
+			ansiToGet = thePackList[i];
+			console.putmsg("\r\n\1h\1g\r\n UPCOMING \1r *** \1y" + ansiToGet.file_location + " \1yOr***\1g BELOW\r\n\r\n\1n");	
+			grabAnsi(ansiToGet.file_location);
+				if(console.inkey(K_UPPER, 500) == "Q"){ break; }
+			showAnsi();
+			console.putmsg("\r\n\1h\1c\r\n THAT WAS \1m *** \1w" + ansiToGet.file_location + " \1m***\1c ABOVE\r\n\r\n\1n");
+		}
+}
 
+//this is  the function that chunks and displays the ansi, throttling it in the process
 function showAnsi(){
 	currentAnsiFileChunk = currentAnsiFile.split("");
 	 while(currentAnsiFileChunk.length > 0) { 
 	 		console.write(currentAnsiFileChunk.splice(0,modemSpeed).join(""));
-                  if(console.inkey(K_UPPER, 1) == "Q"){ 
-                  	break; }
-                   }
-
+                  if(console.inkey(K_UPPER, 1) == "Q"){ break; }
+        }
 }
 
-//console.print(grabAnsi(askForAnsiFromPack("blocktronics_wtf4")));  // a very simple way to ask for a file from a pack
-
-
-//4. http://sixteencolors.net/pack/01ninja/FZ-BLUE.ANS/download -- use file location parameter to get the download URL (dont use 'api' prefix)
-
-//console.print(grabAnsi("/pack/01ninja/FZ-BLUE.ANS/download"));;
-
+//this goes and grabs the ANSI and puts it in a global variable containing the current ANSI
 function grabAnsi(fileLocation){
 	var request = new HTTPRequest();
 	var ansiRoute = siteRoot + fileLocation;
@@ -366,16 +340,4 @@ function grabAnsi(fileLocation){
 	var ansiBody = request.body;
 	currentAnsiFile =  ansiBody;
 	return ansiBody;
-
 }
-
-
-
-//The Other SelectApack function somewhat copied to be more menuOptions
-
-function selectAPack2(packNum){
-		console.putmsg("\r\n\1rSelect a Pack Number.  ");
-		choice = packNum;
-		currentPack = thePacks[choice].name;
-		askForAnsiFromPack(currentPack);
-	}
